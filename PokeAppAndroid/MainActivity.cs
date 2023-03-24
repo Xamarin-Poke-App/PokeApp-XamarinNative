97aa8078f5c5748348195abd3235b5c13353a7e1
adriana <adrianacc.bueno@gmail.com>
2023-03-24 00:57:33 -0400

    Login page added

--- PokeAppAndroid/MainActivity.cs
+++ PokeAppAndroid/MainActivity.cs
@@ -14,6 +14,7 @@ namespace PokeAppAndroid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            StartActivity(typeof(LoginActivity));
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
