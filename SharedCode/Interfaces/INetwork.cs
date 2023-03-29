3a047803fc8531f1fdf1a78bc3f3c6b7086686d3
Israel Aguilar <isra.neri2@gmail.com>
2023-03-29 11:26:33 -0400

    Add Network handler using Xamarin Essectials Connectivity.

---
+++ SharedCode/Interfaces/INetwork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SharedCode.Interfaces
+{
+    public interface INetwork
+    {
+        EventHandler<NetworkChangedEventArgs> NetworkHasChanged { set; get; }
+        void OnNetworkChanged(NetworkChangedEventArgs args);
+    }
+
+    public class NetworkChangedEventArgs : EventArgs
+    {
+        public bool IsConnected { get; set; }
+    }
+}
--- SharedCode/Util/NetworkHandler.cs
+++ SharedCode/Util/NetworkHandler.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using Xamarin.Essentials;
+using SharedCode.Interfaces;
+
 namespace SharedCode.Util
 {
-	public class NetworkHandler
-	{
-		public NetworkHandler()
-		{
-		}
-	}
+    public class NetworkHandler : INetwork
+    {
+        public NetworkHandler(EventHandler<NetworkChangedEventArgs> handler)
+        {
+            NetworkHasChanged += handler;
+            Connectivity.ConnectivityChanged += OnNetworkChanged;
+        }
+
+        public EventHandler<NetworkChangedEventArgs> NetworkHasChanged { get; set; }
+
+        private void OnNetworkChanged(object sender, ConnectivityChangedEventArgs args)
+        {
+            EventHandler<NetworkChangedEventArgs> handler = NetworkHasChanged;
+
+            if (handler == null) return;
+
+            var access = args.NetworkAccess;
+            var profiles = args.ConnectionProfiles;
+            var customArgs = new NetworkChangedEventArgs();
+
+            if (access == NetworkAccess.Internet)
+            {
+                customArgs.IsConnected = true;
+                this.OnNetworkChanged(customArgs);
+                return;
+            }
+
+            customArgs.IsConnected = false;
+            this.OnNetworkChanged(customArgs);
+        }
+
+        public void OnNetworkChanged(NetworkChangedEventArgs args)
+        {
+            NetworkHasChanged.Invoke(this, args);
+        }
+    }
 }
 
