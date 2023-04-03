65659aeca4cb5fd4028dcdfe61d011ae54100d0e
YawarValeriano <yawarvs@gmail.com>
2023-04-03 17:32:38 -0400

    POK-55 Update display of images and add type colors to strings

--- PokeAppAndroid/Adapters/PokemonAdapter.cs
+++ PokeAppAndroid/Adapters/PokemonAdapter.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using Android.Content;
 using Android.Views;
 using Android.Widget;
 using AndroidX.RecyclerView.Widget;
+using SharedCode.Controller;
 using SharedCode.Model;
+using SharedCode.Services;
+using SharedCode.Util;
+using Square.Picasso;
 
 namespace PokeAppAndroid.Adapters
 {
     public class PokemonAdapter : RecyclerView.Adapter
     {
+
         public List<ResultPokemons> pokemomList;
         public event EventHandler<int> ItemClick;
 
@@ -25,7 +31,14 @@ namespace PokeAppAndroid.Adapters
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
             PokemonViewHolder viewHolder = holder as PokemonViewHolder;
-            viewHolder.TvPokemonName.Text = pokemomList[position].name;
+            var value = pokemomList[position];
+            viewHolder.TvPokemonName.Text = value.FormatedName();
+            string pokemonID = value.GetIdFromUrl().ToString();
+
+            viewHolder.TvPokemonNumber.Text = "#" + pokemonID;
+            Picasso.Get()
+                .Load(value.GetPokemonImageURL(pokemonID))
+                .Into(viewHolder.IvPokemonImage);
         }
 
         public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
@@ -38,10 +51,14 @@ namespace PokeAppAndroid.Adapters
         public class PokemonViewHolder : RecyclerView.ViewHolder
         {
             public TextView TvPokemonName { get; set; }
+            public ImageView IvPokemonImage { get; set; }
+            public TextView TvPokemonNumber { get; set; }
 
             public PokemonViewHolder(Android.Views.View card, Action<int> listener) : base(card)
             {
                 TvPokemonName = card.FindViewById<TextView>(Resource.Id.tv_pokemonName);
+                IvPokemonImage = card.FindViewById<ImageView>(Resource.Id.PokemonImage);
+                TvPokemonNumber = card.FindViewById<TextView>(Resource.Id.TvPokemonNumber);
                 card.Click += (sender, e) => listener(base.AbsoluteAdapterPosition);
             }
         }
@@ -51,6 +68,8 @@ namespace PokeAppAndroid.Adapters
             if (ItemClick != null)
                 ItemClick(this, obj);
         }
+
+  
     }
 }
 
--- SharedCode/Model/PokemonSpeciesResponse.cs
+++ SharedCode/Model/PokemonSpeciesResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using SharedCode.Util;
 
 namespace SharedCode.Model
 {
@@ -26,6 +27,24 @@ namespace SharedCode.Model
                 return id;
             }
             return -1;
+        }
+        
+        public string FormatedName()
+        {
+            var list = name.Split('-');
+            string newName = "";
+            for (var i = 0; i < list.Length; i++)
+            {
+                var str = list[i];
+                newName += (char.ToUpper(str[0]) + str.Substring(1)) + " ";
+            }
+
+            return newName.Trim();
+        }
+
+        public string GetPokemonImageURL(string id)
+        {
+            return $"{Constants.PokemonArtWorksImagesBaseAddress}{id}.png";
         }
     }
 }
