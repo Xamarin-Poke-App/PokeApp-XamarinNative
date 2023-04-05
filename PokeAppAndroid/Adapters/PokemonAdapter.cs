9e7cc5511bac67138dd0ca03914fc71ee51ae25c
YawarValeriano <yawarvs@gmail.com>
2023-04-04 21:33:42 -0400

    POK-55 Add extensions for add drawables to views

--- PokeAppAndroid/Adapters/PokemonAdapter.cs
+++ PokeAppAndroid/Adapters/PokemonAdapter.cs
@@ -6,6 +6,7 @@ using Android.Widget;
 using AndroidX.ConstraintLayout.Widget;
 using AndroidX.Core.Content;
 using AndroidX.RecyclerView.Widget;
+using PokeAppAndroid.Utils;
 using SharedCode.Controller;
 using SharedCode.Model;
 using SharedCode.Services;
@@ -36,20 +37,17 @@ namespace PokeAppAndroid.Adapters
             PokemonViewHolder viewHolder = holder as PokemonViewHolder;
             var value = pokemomList[position];
             string pokemonID = value.ID;
-            var coloredDrwawable = GetDrawableWithColor(viewHolder.Container.Context, Resource.Drawable.rounded_background, value.Types[0], true);
-
-            viewHolder.Container.Background = coloredDrwawable;
 
             viewHolder.FirstType.Text = value.Types[0].ToString();
-            var firstTypeDrawable = GetDrawableWithColor(viewHolder.FirstType.Context, Resource.Drawable.rounded_background_type, value.Types[0]);
-            viewHolder.FirstType.Background = firstTypeDrawable;
+            viewHolder.Container.SetDrawableBackgroundForType(value.Types[0]);
+
             if (value.Types.Count > 1)
             {
                 viewHolder.SecondType.Visibility = ViewStates.Visible;
                 viewHolder.SecondType.Text = value.Types[1].ToString();
-                var secondTypeDrawable = GetDrawableWithColor(viewHolder.SecondType.Context, Resource.Drawable.rounded_background_type, value.Types[1]);
-                viewHolder.SecondType.Background = secondTypeDrawable;
+                viewHolder.SecondType.SetDrawableBackgroundForType(value.Types[1]);
             }
+
             viewHolder.TvPokemonName.Text = value.Name;
             viewHolder.TvPokemonNumber.Text = "#" + pokemonID;
             Picasso.Get()
@@ -90,63 +88,7 @@ namespace PokeAppAndroid.Adapters
         {
             if (ItemClick != null)
                 ItemClick(this, obj);
-        }
-
-        private Android.Graphics.Drawables.Drawable GetDrawableWithColor(Context context, int drawable, Enums.PokemonTypes type, bool useAlpha = false)
-        {
-            int primariIdColor = getColorForType(type);
-            var background = ContextCompat.GetDrawable(context, drawable);
-            var color = new Android.Graphics.Color(ContextCompat.GetColor(context, primariIdColor));
-            if (useAlpha)
-                color.A = (byte)(0.80 * 255);
-            background.SetTint(color);
-            return background;
-        }
-
-        private int getColorForType(Enums.PokemonTypes type)
-        {
-            switch (type)
-            {
-                case Enums.PokemonTypes.Bug:
-                    return Resource.Color.bug;
-                case Enums.PokemonTypes.Dark:
-                    return Resource.Color.dark;
-                case Enums.PokemonTypes.Dragon:
-                    return Resource.Color.dragon;
-                case Enums.PokemonTypes.Electric:
-                    return Resource.Color.electric;
-                case Enums.PokemonTypes.Fairy:
-                    return Resource.Color.fairy;
-                case Enums.PokemonTypes.Fighting:
-                    return Resource.Color.fighting;
-                case Enums.PokemonTypes.Fire:
-                    return Resource.Color.fire;
-                case Enums.PokemonTypes.Flying:
-                    return Resource.Color.flying;
-                case Enums.PokemonTypes.Ghost:
-                    return Resource.Color.ghost;
-                case Enums.PokemonTypes.Grass:
-                    return Resource.Color.grass;
-                case Enums.PokemonTypes.Ground:
-                    return Resource.Color.ground;
-                case Enums.PokemonTypes.Ice:
-                    return Resource.Color.ice;
-                case Enums.PokemonTypes.Normal:
-                    return Resource.Color.normal;
-                case Enums.PokemonTypes.Poison:
-                    return Resource.Color.poison;
-                case Enums.PokemonTypes.Psychic:
-                    return Resource.Color.psychic;
-                case Enums.PokemonTypes.Rock:
-                    return Resource.Color.rock;
-                case Enums.PokemonTypes.Steel:
-                    return Resource.Color.steel;
-                case Enums.PokemonTypes.Water:
-                    return Resource.Color.water;
-                default:
-                    return Resource.Color.colorAccent;
-            }
-        }
+        }  
     }
 }
 
---
+++ PokeAppAndroid/Utils/ViewExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Android.Widget;
+using AndroidX.ConstraintLayout.Widget;
+using AndroidX.Core.Content;
+using SharedCode.Util;
+
+namespace PokeAppAndroid.Utils
+{
+	public static class ViewExtensions
+	{
+		public static TextView SetDrawableBackgroundForType(this TextView view, Enums.PokemonTypes type)
+		{
+            var colorID = GetColorForType(type);
+            var background = ContextCompat.GetDrawable(view.Context, Resource.Drawable.rounded_background_type);
+            var color = new Android.Graphics.Color(ContextCompat.GetColor(view.Context, colorID));
+            background.SetTint(color);
+            view.Background = background;
+            return view;
+        }
+
+
+        public static ConstraintLayout SetDrawableBackgroundForType(this ConstraintLayout view, Enums.PokemonTypes type)
+        {
+            var colorID = GetColorForType(type);
+            var background = ContextCompat.GetDrawable(view.Context, Resource.Drawable.rounded_background);
+            var color = new Android.Graphics.Color(ContextCompat.GetColor(view.Context, colorID));    
+            color.A = (byte)(0.80 * 255);
+            background.SetTint(color);
+            view.Background = background;
+            return view;
+        }
+
+        private static int GetColorForType(Enums.PokemonTypes type)
+        {
+            switch (type)
+            {
+                case Enums.PokemonTypes.Bug:
+                    return Resource.Color.bug;
+                case Enums.PokemonTypes.Dark:
+                    return Resource.Color.dark;
+                case Enums.PokemonTypes.Dragon:
+                    return Resource.Color.dragon;
+                case Enums.PokemonTypes.Electric:
+                    return Resource.Color.electric;
+                case Enums.PokemonTypes.Fairy:
+                    return Resource.Color.fairy;
+                case Enums.PokemonTypes.Fighting:
+                    return Resource.Color.fighting;
+                case Enums.PokemonTypes.Fire:
+                    return Resource.Color.fire;
+                case Enums.PokemonTypes.Flying:
+                    return Resource.Color.flying;
+                case Enums.PokemonTypes.Ghost:
+                    return Resource.Color.ghost;
+                case Enums.PokemonTypes.Grass:
+                    return Resource.Color.grass;
+                case Enums.PokemonTypes.Ground:
+                    return Resource.Color.ground;
+                case Enums.PokemonTypes.Ice:
+                    return Resource.Color.ice;
+                case Enums.PokemonTypes.Normal:
+                    return Resource.Color.normal;
+                case Enums.PokemonTypes.Poison:
+                    return Resource.Color.poison;
+                case Enums.PokemonTypes.Psychic:
+                    return Resource.Color.psychic;
+                case Enums.PokemonTypes.Rock:
+                    return Resource.Color.rock;
+                case Enums.PokemonTypes.Steel:
+                    return Resource.Color.steel;
+                case Enums.PokemonTypes.Water:
+                    return Resource.Color.water;
+                default:
+                    return Resource.Color.colorAccent;
+            }
+        }
+    }
+}
+
